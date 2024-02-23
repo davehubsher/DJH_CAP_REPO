6390579b5213c34b63a433d0a8e134f6d08cc22d
David Hubsher <davehubsher@gmail.com>
2024-02-23 22:04:49 +0000

    Localization Commit

--- srv/cat-services.js
+++ srv/cat-services.js
@@ -7,10 +7,25 @@ module.exports = cds.service.impl(function () {
     })
     
     this.after(['CREATE', 'UPDATE', 'DELETE'], [Products], async (Product, req) => {
-        const header = req.data
-        req.on('succeeded', () => {
-            global.it || console.log(`< emitting: product_Changed ${Product.ID}`)
-            this.emit('prod_Change', header)
-        })
-    })
+        const header = req.data
+        req.on('succeeded', () => {
+            global.it || console.log(`< emitting: product_Changed ${Product.ID}`)
+        this.emit('prod_Change', header)
+        })
+    })
+this.on('get_supplier_info', async () => {
+        try {
+            const db = await cds.connect.to('db')
+            const dbClass = require("sap-hdbext-promisfied")
+            let dbConn = new dbClass(await dbClass.createConnection(db.options.credentials))
+            const hdbext = require("@sap/hdbext")
+            const sp = await dbConn.loadProcedurePromisified(hdbext, null, 'get_supplier_info')
+            const output = await dbConn.callProcedurePromisified(sp, [])
+            console.log(output.results)
+            return output.results
+        } catch (err) {
+            console.error(err)
+            return
+        }
+    })
 })
